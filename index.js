6229adf115f9e3e6974b93b2596d213e3233df56
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-11-08 21:52:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(artists, instruments) {
+    var artists = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    var array = [];
+  for (let i = 0; i < instruments.length; i++){
+    array.push(artists[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
+var array = [];
+  while (i < facts.length);
+  var i = 0;
+  {
+    array.push(facts[i] + "!!!");
+    i++;
+    }
+  return array;
+}
+
+function iLoveTheBeatles () {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+  }
+  while (array < 15);
+  return array;
+}
